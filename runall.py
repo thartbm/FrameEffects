b466d2b1a00453b01f28712ad408876ea8614b82
Marius 't Hart <thartbm@gmail.com>
2024-11-27 12:40:08 -0500

    changes to path (creating data folders)

--- runall.py
+++ runall.py
@@ -46,19 +46,19 @@ def getParticipant(cfg, ID=np.nan, check_path=True):
 
     # set up folder's for groups and participants to store the data
     if check_path:
-        for thisPath in ['data', 'data/exp_%d'%(cfg['expno']), 'data/exp_%d/p%03d'%(cfg['expno'],cfg['ID'])]:
+        for thisPath in ['data', 'data/exp_%d'%(cfg['exp_version']), 'data/exp_%d/p%03d'%(cfg['exp_version'],cfg['ID'])]:
             if os.path.exists(thisPath):
                 if not(os.path.isdir(thisPath)):
                     os.makedirs
                     sys.exit('"%s" should be a folder'%(thisPath))
                 else:
                     # if participant folder exists, don't overwrite existing data?
-                    if (thisPath == 'data/exp_%d/p%03d'%(cfg['expno'],cfg['ID'])):
+                    if (thisPath == 'data/exp_%d/p%03d'%(cfg['exp_version'],cfg['ID'])):
                         sys.exit('participant already exists (crash recovery not implemented)')
             else:
                 os.mkdir(thisPath)
 
-        cfg['datadir'] = 'data/exp_%d/p%03d/'%(cfg['expno'],cfg['ID'])
+        cfg['datadir'] = 'data/exp_%d/p%03d/'%(cfg['exp_version'],cfg['ID'])
 
     # we need to seed the random number generator:
     random.seed(99999 * IDno)
@@ -69,9 +69,9 @@ def getParticipant(cfg, ID=np.nan, check_path=True):
 
 def runall(demo=False):
 
-    expno=1
+    exp_version=1
     if demo:
-        expno=2
+        exp_version=2
 
     all_cfg = {}
     all_cfg = getParticipant(all_cfg, check_path=False)
@@ -92,28 +92,45 @@ def runall(demo=False):
     # because we set the seed with participant ID * 99999
     random.shuffle(experiments)
 
+<<<<<<< HEAD
+    for exp_idx in range(len(experiments)):
+
+        expcode = experiments[exp_idx]
+
+        if expcode == 'A1':
+            print('\nrunning A1: Anaglyph\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
     for exp_no in range(len(experiments)):
 
         expcode = experiments[exp_no]
 
         if expcode == 'A1':
             print('\nrunning A1: Anaglyph\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
 
             os.chdir('A1_Anaglyph/exp/')
 
             # run it, first calibration:
             os.system('ipython3 red_cyan_calibration.py %d'%(all_cfg['ID']))
             # then the actual task:
+<<<<<<< HEAD
+            os.system('ipython3 frame_depth.py %d %d'%(exp_version, all_cfg['ID']))
+=======
             os.system('ipython3 frame_depth.py %d %d'%(expno, all_cfg['ID']))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
 
             os.chdir('../..')
             print('\n[exp A1 done]\n')
         if expcode == 'A2':
+<<<<<<< HEAD
+            print('\nrunning A2: Probe Distance\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
             print('\nrunning A2: Probe Distance\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
             os.chdir('A2_ProbeDistance/exp/')
 
             # run it!
-            os.system('ipython3 frame_space.py %d %d'%(expno, all_cfg['ID']))
+            os.system('ipython3 frame_space.py %d %d'%(exp_version, all_cfg['ID']))
 
             os.chdir('../..')
             print('\n[exp A2 done]\n')
@@ -121,56 +138,76 @@ def runall(demo=False):
         #    os.chdir('A3_FullFrame/exp/')
         #    import something as curr_exp
              # run it!
-             #curr_exp.run_exp(setup='laptop', expno=1, ID=cfg['ID'])
+             #curr_exp.run_exp(setup='laptop', exp_version=1, ID=cfg['ID'])
         #    os.chdir('../..')
         #    print('[exp A3 done]')
 
         if expcode == 'B1':
+<<<<<<< HEAD
+            print('\nrunning B1: Apparent Lag\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
             print('\nrunning B1: Apparent Lag\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
             os.chdir('B1_ApparentLag/exp/')
 
             # run it!
-            os.system('ipython3 frame_apparent.py %d %d'%(expno, all_cfg['ID']))
+            os.system('ipython3 frame_apparent.py %d %d'%(exp_version, all_cfg['ID']))
 
             os.chdir('../..')
             print('\n[exp B1 done]\n')
         if expcode == 'B2':
+<<<<<<< HEAD
+            print('\nrunning B2: Pre/Post Diction\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
             print('\nrunning B2: Pre/Post Diction\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
             os.chdir('B2_PreDiction/exp/')
 
             # run it!
-            os.system('ipython3 frame_time.py %d %d'%(expno, all_cfg['ID']))
+            os.system('ipython3 frame_time.py %d %d'%(exp_version, all_cfg['ID']))
 
             os.chdir('../..')
             print('\n[exp B2 done]\n')
 
         if expcode == 'C1':
+<<<<<<< HEAD
+            print('\nrunning C1: Self Motion\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
             print('\nrunning C1: Self Motion\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
             os.chdir('C1_SelfMotion/exp/')
 
             # run it!
-            os.system('ipython3 frame_movement.py %d %d'%(expno, all_cfg['ID']))
+            os.system('ipython3 frame_movement.py %d %d'%(exp_version, all_cfg['ID']))
 
             os.chdir('../..')
             print('\n[exp C1 done]\n')
         if expcode == 'C2':
+<<<<<<< HEAD
+            print('\nrunning C2: Dot-Texture Motion\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
             print('\nrunning C2: Dot-Texture Motion\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
             os.chdir('C2_TextureMotion/exp/')
 
             # run it!
-            os.system('ipython3 frame_background2.py %d %d'%(expno, all_cfg['ID']))
+            os.system('ipython3 frame_background2.py %d %d'%(exp_version, all_cfg['ID']))
 
             os.chdir('../..')
             print('\n[exp C2 done]\n')
         if expcode == 'C3':
+<<<<<<< HEAD
+            print('\nrunning C3: Dot-Texture Perceived Frame Motion\n(task %d / %d)\n\n'%(exp_idx+1,len(experiments)))
+=======
             print('\nrunning C3: Dot-Texture Perceived Frame Motion\n(task %d / %d)\n\n'%(exp_no,len(experiments)))
+>>>>>>> c1d88d6dcceb7ac459d6cda60ce49252a86cdeb7
             os.chdir('C3_PerceivedMotion/exp/')
 
             # run it!
-            os.system('ipython3 frame_motion.py %d %d'%(expno, all_cfg['ID']))
+            os.system('ipython3 frame_motion.py %d %d'%(exp_version, all_cfg['ID']))
 
             os.chdir('../..')
             print('\n[exp C3 done]\n')
 
 
-runall()
+runall(demo=True)
